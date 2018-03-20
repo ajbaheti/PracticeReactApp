2fd2f129a99a3ab5160888ca495d1ed378a345ef
Ashish Baheti <ajb9736@rit.edu>
2018-03-20 00:35:58 -0400

    conditional operator if

--- src/App.js
+++ src/App.js
@@ -49,6 +49,26 @@ class App extends Component {
             outline: 'none'
         };
 
+        let persons = null;
+
+        if (this.state.showPersons) {
+            persons = (
+                <div>
+                    <Person
+                        name={this.state.persons[0].name}
+                        age={this.state.persons[0].age}/>
+                    <Person
+                        name={this.state.persons[1].name}
+                        age={this.state.persons[1].age}
+                        click={this.switchNameHandler.bind(this, 'AB')}
+                        changed={this.nameChangedHandler}>My Hobbies: Numismatic</Person>
+                    <Person
+                        name={this.state.persons[2].name}
+                        age={this.state.persons[2].age}/>
+                </div>
+            );
+        }
+
         return (
             <div className="App">
                 <h1> I'm a react app</h1>
@@ -56,25 +76,7 @@ class App extends Component {
                 <button
                     style={style}
                     onClick={this.togglePersonHandler}>Toggle Persons</button>
-                {
-                    this.state.showPersons ?
-                        <div>
-                            <Person
-                                name={this.state.persons[0].name}
-                                age={this.state.persons[0].age}
-                            />
-                            <Person
-                                name={this.state.persons[1].name}
-                                age={this.state.persons[1].age}
-                                click={this.switchNameHandler.bind(this, 'AB')}
-                                changed={this.nameChangedHandler}>My Hobbies: Numismatic
-                            </Person>
-                            <Person
-                                name={this.state.persons[2].name}
-                                age={this.state.persons[2].age}
-                            />
-                        </div>  : null
-                }
+                {persons}
             </div>
         );
     }
