7e3612b77e30f1ef5da775ee0aa67cdf43fc3fa2
Ashish Baheti <ajb9736@rit.edu>
2018-03-21 12:37:52 -0400

    dynamic styling

--- src/App.js
+++ src/App.js
@@ -44,7 +44,8 @@ class App extends Component {
 
     render() {
         const style = {
-            backgroundColor: 'grey',
+            backgroundColor: 'green',
+            color: 'white',
             font: 'inherit',
             border: '1px solid red',
             padding: '8px',
@@ -67,6 +68,7 @@ class App extends Component {
                     })}
                 </div>
             );
+            style.backgroundColor = 'red';
         }
 
         return (
