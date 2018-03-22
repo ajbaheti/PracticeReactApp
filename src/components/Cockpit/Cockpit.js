e8876b47ce29e3f53950696ec71d2705e3973eef
Ashish Baheti <ajb9736@rit.edu>
2018-03-22 18:21:06 -0400

    returning adjacent elements in render

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import classes from './Cockpit.css';
+import Aux from '../../hoc/Aux';
 
 const Cockpit = (props) => {
-    let btnClass = '';
+    let btnClass = classes.Button;
     if(props.showPersons){
-        btnClass = classes.Red;
+        btnClass = [classes.Button,classes.Red].join(' ');
     }
 
     const assignedClasses = [];
@@ -17,13 +18,13 @@ const Cockpit = (props) => {
     }
 
     return (
-        <div className={classes.Cockpit}>
+        <Aux>
             <h1> {props.appTitle} </h1>
             <p className={assignedClasses.join(' ')}>This is really working...!!!</p>
             <button
                 className={btnClass}
                 onClick={props.clicked}>Toggle Persons</button>
-        </div>
+        </Aux>
     );
 };
 
